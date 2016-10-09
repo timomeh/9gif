a5c65192d1c808673d904eb5114b7796f1e3f59d
Timo Mämecke <timo@maemecke.com>
2016-10-10 00:22:23 +0200

    Comment everything

--- cli.js
+++ cli.js
@@ -13,27 +13,39 @@ const args = new Args(process.argv)
 const spinner = new Spinner()
 spinner.setSpinnerString(18)
 
-args.check()
+
+args.check() /* Check arguments for validity and sanitize them*/
 .then(() => {
+  /* Parse the 9GAG page for a mp4 link */
+
   spinner.setSpinnerTitle(`${chalk.dim('%s')} Searching GIF`)
   spinner.start()
   const gagPage = new NineGag({ url: args.url })
   return gagPage.parse()
 })
 .then(mp4Url => {
+  /* Send the mp4 to Giphy and generate a read gif */
+
   spinner.setSpinnerTitle(`${chalk.dim('%s')} Generating GIF`)
   const giphy = new Giphy({ mp4Url, source: args.url })
   return giphy.generate()
 })
 .then(gifUrl => {
+  /* Download the gif from giphy to local path */
+
   spinner.setSpinnerTitle(`${chalk.dim('%s')} Downloading GIF`)
   return Utils.download(gifUrl, args.dest)
 })
 .then(() => {
+  /* Success! */
+
   spinner.stop(true)
   console.log(chalk.green(`✓ Saved GIF to ${args.dest}`))
 })
 .catch(e => {
+  /* Complete error handling */
+  // TODO: Format nice errors
+
   spinner.stop(true)
   console.log(chalk.red(`× Error: ${e}`))
   process.exit(1)
