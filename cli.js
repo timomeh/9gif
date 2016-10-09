5abdf4c4708dcf5fce61d9534c7c8a6866d7c5e3
Timo Mämecke <timo@maemecke.com>
2016-10-09 23:58:32 +0200

    Refactored everything

--- cli.js
+++ cli.js
@@ -2,19 +2,39 @@
 
 'use strict'
 
+const Spinner = require('cli-spinner').Spinner
+const chalk = require('chalk')
 const Args = require('./lib/args')
-const Parser = require('./lib/parser')
-const Gif = require('./lib/gif')
+const NineGag = require('./lib/nine-gag')
+const Giphy = require('./lib/giphy')
+const Utils = require('./lib/utils')
 
-try {
-  var args = new Args(process.argv)
-} catch (e) {
-  console.log(e)
+const args = new Args(process.argv)
+const spinner = new Spinner()
+spinner.setSpinnerString(18)
+
+args.check()
+.then(() => {
+  spinner.setSpinnerTitle(`${chalk.dim('%s')} Searching GIF`)
+  spinner.start()
+  const gagPage = new NineGag({ url: args.url })
+  return gagPage.parse()
+})
+.then(mp4Url => {
+  spinner.setSpinnerTitle(`${chalk.dim('%s')} Generating GIF`)
+  const giphy = new Giphy({ mp4Url, source: args.url })
+  return giphy.generate()
+})
+.then(gifUrl => {
+  spinner.setSpinnerTitle(`${chalk.dim('%s')} Downloading GIF`)
+  return Utils.download(gifUrl, args.dest)
+})
+.then(() => {
+  spinner.stop(true)
+  console.log(chalk.green(`✓ Saved GIF to ${args.dest}`))
+})
+.catch(e => {
+  spinner.stop(true)
+  console.log(chalk.red(`× Error: ${e}`))
   process.exit(1)
-}
-
-const parser = new Parser(args.url)
-
-parser.getGifSrc()
-.then(src => (new Gif(src, args.url, args.dest).convertVideo()))
-.catch(e => { throw e })
+})
